0d99b9b985b628669d44ba53ce3a8a06c07c4ee0
Anton <2hammer90@mail.ru>
2017-03-23 00:11:45 +0300

    sidebar markup, updated angular-cli version

--- src/app/shared/icons-sprite/icons-sprite.component.ts
+++ src/app/shared/icons-sprite/icons-sprite.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-icons-sprite',
-  templateUrl: './icons-sprite.component.html'
+  templateUrl: './icons-sprite.component.html',
+  styles: [':host {display: none}']
 })
 export class IconsSpriteComponent {
 
